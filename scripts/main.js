7afb9ce9eb137b00d7421d46c75f63d4d11aa3b6
RichardDeBrito <richartdebrito2@gmail.com>
2025-11-14 11:24:26 -0300

    Mais ajustes na responsividade, modularizado o java script e iniciado o consumo da API.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,14 @@
-const hamburguer = document.getElementById('hamburguer');
-        const menu = document.getElementById('menu');
-        const overlay = document.getElementById('overlay');
-        
-        hamburguer.addEventListener('click', () => {
-            hamburguer.classList.toggle('active');
-            menu.classList.toggle('active');
-            overlay.classList.toggle('active');
-        });
-        
-        overlay.addEventListener('click', () => {
-            hamburguer.classList.remove('active');
-            menu.classList.remove('active');
-            overlay.classList.remove('active');
-        });
+const API_KEY = 'b950b2f8751b9d6c2cf1bf2e45f1dd11';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+async function loadGenres() {
+    try {
+        const response = await fetch (`${BASE_URL}/genre/movie/list?api_key${API_KEY}&language=pt-BR`);
+        const data = await response.json();
+
+        populateGenreSelect(data.genres);
+    } catch (error) {
+        console.error('Erro ao carregar gêneros', error);
+    }
+}
